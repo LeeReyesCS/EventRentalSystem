d51bc8e8d8fef25633c7b9f1c8b06e882144b0bc
Christian Mojica <christian.f.mojica@gmail.com>
2023-04-29 19:23:44 -0400

    Fixed grid content in homepage

--- src/components/HomepageContent.js
+++ src/components/HomepageContent.js
@@ -8,35 +8,30 @@ import GabbyHomePagePic4 from "../images/Gabby_Homepage_Pic4.jpg";
 const HomepageContent = () => {
   return (
     <div className="container-Content-Home">
-      <div className="top-half">
-        <img src={GabbyHomePagePic1} alt="" />
+      <img src={GabbyHomePagePic1} alt="" />
+      <h1>
+        Proudly Serving Connecticut & the Hudson Valley <br /> ~
+        <br /> Orgullosamente sirviendo a Connecticut y al Valle de Hudson
+      </h1>
+      <img src={GabbyHomePagePic2} alt="" />
+      <img src={GabbyHomePagePic3} alt="" />
+      <div className="offer-text">
         <h1>
-          Proudly Serving Connecticut & the Hudson Valley <br /> ~
-          <br /> Orgullosamente sirviendo a Connecticut y al Valle de Hudson
+          What We Offer <br /> ~ <br /> Lo Que Ofrecemos
         </h1>
-        <img src={GabbyHomePagePic2} alt="" />
+        <ul className="offer-items">
+          <li className="offer-item">Birthdays || Cumpleaños</li>
+          <li className="offer-item">Weddings || Bodas</li>
+          <li className="offer-item">Baptisms || Bautismos</li>
+          <li className="offer-item">Sweet 16 || Quinceañeras</li>
+          <li className="offer-item">Receptions || Recepciones</li>
+          <li className="offer-item">Graduations || Graduaciones</li>
+          <li className="offer-item">Reunions || Reuniones</li>
+          <li className="offer-item">Baby Showers</li>
+          <li className="offer-item">...and more!</li>
+        </ul>
       </div>
-      <div className="bottom-half">
-        <img style={{ height: "250px" }} src={GabbyHomePagePic3} alt="" />
-        <div className="offer-text">
-          <h1>
-            What We Offer <br /> ~ <br /> Lo Que Ofrecemos
-          </h1>
-          <ul className="offer-items">
-            <li className="offer-item">Birthdays || Cumpleaños</li>
-            <li className="offer-item">Weddings || Bodas</li>
-            <li className="offer-item">Baptisms || Bautismos</li>
-            <li className="offer-item">Sweet 16 || Quinceañeras</li>
-            <li className="offer-item">Receptions || Recepciones</li>
-            <li className="offer-item">Graduations || Graduaciones</li>
-            <li className="offer-item">Reunions || Reuniones</li>
-            <li className="offer-item">Baby Showers</li>
-            <li className="offer-item">...and more!</li>
-          </ul>
-        </div>
-
-        <img style={{ height: "250px" }} src={GabbyHomePagePic4} alt="" />
-      </div>
+      <img src={GabbyHomePagePic4} alt="" />
     </div>
   );
 };
